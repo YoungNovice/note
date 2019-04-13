215e5ded34709fc7b2cb0454398cf0ad2e6a731c
AlanYoung <2306915539@qq.com>
2019-04-13 16:31:33 +0800

    update some notes

--- emacs/load-path.md
+++
@@ -1,4 +0,0 @@
-(add-to-list 'load-path "/usr/share/emacs/site-lisp")
-(require 'linum)
-(global-linum-mode t)
-
---
+++ jinzhi.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void) {
+	int a = -100;
+	printf("%#X\n", a);
+
+	int b = 0xFFFFFFEF;
+	printf("%d\n", b);
+	return 0;
+}
